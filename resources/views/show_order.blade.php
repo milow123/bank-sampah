908450f6c9e5d7bfedc4fc26799d1a752cf053f0
milow <haryoprabu.sd@gmail.com>
2023-11-03 17:49:09 +0700

    commit

---
+++ resources/views/show_order.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Order Detail') }}</div>
+
+                    @php
+                        $total_price = 0;
+                    @endphp
+
+                    <div class="card-body">
+                        <h5 class="card-title">Order ID {{ $order->id }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted"> {{ $order->user->name }}</h6>
+
+                        @if ($order->is_paid == true)
+                            <p class="card-text">Paid</p>
+                        @else
+                            <p class="card-text">Unpaid</p>
+                        @endif
+                        <hr>
+                        @foreach ($order->transactions as $transaction)
+                            <p>{{ $transaction->product->name }} - {{ $transaction->amount }} Kg</p>
+                            @php
+                                $total_price += $transaction->product->price * $transaction->amount;
+                            @endphp
+                        @endforeach
+                        <hr>
+                        <p>Total: Rp{{ $total_price }}</p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\OrderController;
+use App\Models\Product;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/product/create', [ProductController::class, 'create_product'])->name('create_product');
+Route::post('/product/create', [ProductController::class, 'type_product'])->name('type_product');
+Route::get('/product', [ProductController::class, 'index_product'])->name('index_product');
+Route::get('/product/{product}', [ProductController::class, 'show_product'])->name('show_product');
+Route::get('/product/{product}/edit', [ProductController::class, 'edit_product'])->name('edit_product');
+Route::patch('/product/{product}/update', [ProductController::class, 'update_product'])->name('update_product');
+Route::delete('/product/{product}', [ProductController::class, 'delete_product'])->name('delete_product');
+
+
+Route::post('/cart/{product}', [CartController::class, 'add_to_cart'])->name('add_to_cart');
+Route::get('/cart', [CartController::class, 'show_cart'])->name('show_cart');
+Route::patch('/cart/{cart}', [CartController::class, 'update_cart'])->name('update_cart');
+Route::delete('/cart/{cart}', [CartController::class, 'delete_cart'])->name('delete_cart');
+
+Route::post('/checkout', [OrderController::class, 'checkout'])->name('checkout');
+Route::get('/order', [OrderController::class, 'index_order'])->name('index_order');
+Route::get('/order/{order}', [OrderController::class, 'show_order'])->name('show_order');
+Route::post('/order/{order}/comfirm', [OrderController::class, 'confirm_payment'])->name('confirm_payment');
